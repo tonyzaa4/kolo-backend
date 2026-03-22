6d45f4a9b146f85fe45615fe0c7c89691eb60c12
Echoes <65855527+EchoesOfShadow@users.noreply.github.com>
2026-03-22 22:32:39 +0200

    Integrate custom exception handlers in FastAPI app
    Added custom exception handlers for HTTP exceptions and validation errors.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+
 from routers import users
 from database import engine
 import models
 
-# Створюємо таблиці, якщо їх немає (про всяк випадок)
-#models.Base.metadata.create_all(bind=engine)
+# Імпорт хендлерів
+from app.exceptions import (
+    http_exception_handler,
+    not_found_handler,
+    validation_exception_handler
+)
 
 app = FastAPI(title="Kolo API")
 
-# Підключаємо логіку з окремих файлів
+# Підключаємо exception handlers
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(404, not_found_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+
+# Підключаємо роутери
 app.include_router(users.router)
 
 @app.get("/")
